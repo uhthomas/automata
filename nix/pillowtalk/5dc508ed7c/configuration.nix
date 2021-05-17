f2b09532c6e0404dc649bad1ad8a2acc97ecc5f0
Thomas <9749173+uhthomas@users.noreply.github.com>
2021-05-17 02:52:41 +0100

    colmena/pillowtalk/5dc508ed7c: Use static IP address

--- nix/pillowtalk/5dc508ed7c/configuration.nix
+++ nix/pillowtalk/5dc508ed7c/configuration.nix
@@ -13,6 +13,8 @@
   };
 
   networking = {
+    defaultGateway = "10.0.0.1";
+    nameservers = [ "1.1.1.1" "1.0.0.1" "8.8.8.8" "8.8.4.4" ];
     bonds.bond0 = {
       interfaces = [ "enp6s0f0" "enp6s0f1" ];
       driverOptions = {
@@ -27,7 +29,10 @@
       eno2.useDHCP = true;
       eno3.useDHCP = true;
       eno4.useDHCP = true;
-      bond0.useDHCP = true;
+      bond0.ipv4.addresses = [{
+        address = "10.0.0.2";
+        prefixLength = 24;
+      }];
     };
   };
 
