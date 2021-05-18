7303f314f445e9aeb5e0d0ee39c6c4c507f8c43b
Thomas <9749173+uhthomas@users.noreply.github.com>
2021-05-18 19:02:14 +0100

    pillowtalk/5dc508ed7c: Enable Kubernetes (master)

--- nix/pillowtalk/5dc508ed7c/configuration.nix
+++ nix/pillowtalk/5dc508ed7c/configuration.nix
@@ -37,4 +37,16 @@
   };
 
   system.stateVersion = "20.09";
+
+  services.kubernetes = {
+    roles = [ "master" "node" ];
+    masterAddress = "api.kube";
+    apiserverAddress = "https://api.kube:6443";
+    easyCerts = true;
+    apiserver = {
+      securePort = 6443;
+      advertiseAddress = "10.0.0.2";
+    };
+    addons.dns.enable = true;
+  };
 }
