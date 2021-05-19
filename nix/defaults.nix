3a4372fc6ea89414f7178d68a6d329f9a368bed3
Thomas <9749173+uhthomas@users.noreply.github.com>
2021-05-19 01:54:13 +0100

    all: Add htop

--- nix/defaults.nix
+++ nix/defaults.nix
@@ -11,7 +11,7 @@
   };
 
   environment = {
-    systemPackages = with pkgs; [ tailscale vim ];
+    systemPackages = with pkgs; [ htop tailscale vim ];
     variables = {
       EDITOR = "vim";
       VISUAL = "vim";
