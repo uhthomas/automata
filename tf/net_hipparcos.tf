80e5d8546115c589e14babca08b1adbc671f4ea5
Thomas Way <thomas@6f.io>
2023-10-23 23:27:59 +0100

    chore(tf): add extra fastmail records

--- tf/net_hipparcos.tf
+++ tf/net_hipparcos.tf
@@ -6,6 +6,107 @@ resource "cloudflare_zone" "terraform_managed_resource_994692a638e9a8c4ae552a9e6
   zone       = "hipparcos.net"
 }
 
+resource "cloudflare_record" "terraform_managed_resource_f7e7d606ea20e6a29c7e268c7715b74e" {
+  name    = "hipparcos.net"
+  proxied = true
+  ttl     = 1
+  type    = "AAAA"
+  value   = "100::"
+  zone_id = "994692a638e9a8c4ae552a9e6dacb609"
+}
+
+# Fastmail
+#
+# https://www.fastmail.help/hc/en-us/articles/360060591153-Manual-DNS-configuration
+
+resource "cloudflare_record" "terraform_managed_resource_9a01f57d2026a498ba69c6e55b25e001" {
+  name     = "hipparcos.net"
+  priority = 10
+  proxied  = false
+  ttl      = 1
+  type     = "MX"
+  value    = "in1-smtp.messagingengine.com"
+  zone_id  = "994692a638e9a8c4ae552a9e6dacb609"
+}
+
+resource "cloudflare_record" "terraform_managed_resource_80a3b9351b70b6c09085329d03c355ab" {
+  name     = "hipparcos.net"
+  priority = 20
+  proxied  = false
+  ttl      = 1
+  type     = "MX"
+  value    = "in2-smtp.messagingengine.com"
+  zone_id  = "994692a638e9a8c4ae552a9e6dacb609"
+}
+
+## Subdomain Mail
+
+resource "cloudflare_record" "terraform_managed_resource_6bc996fbe9aa45078bf34305ad1b6f50" {
+  name     = "*"
+  priority = 10
+  proxied  = false
+  ttl      = 1
+  type     = "MX"
+  value    = "in1-smtp.messagingengine.com"
+  zone_id  = "994692a638e9a8c4ae552a9e6dacb609"
+}
+
+resource "cloudflare_record" "terraform_managed_resource_8ce67ae8f8520e22b15daf5af245c5a3" {
+  name     = "*"
+  priority = 20
+  proxied  = false
+  ttl      = 1
+  type     = "MX"
+  value    = "in2-smtp.messagingengine.com"
+  zone_id  = "994692a638e9a8c4ae552a9e6dacb609"
+}
+
+## Webmail Login Portal
+
+resource "cloudflare_record" "terraform_managed_resource_29190c165c26da1d92c7858b74dab4d8" {
+  name    = "mail"
+  proxied = true
+  ttl     = 1
+  type    = "CNAME"
+  value   = "mail.fastmail.com"
+  zone_id = "994692a638e9a8c4ae552a9e6dacb609"
+}
+
+### Allow mail at subdomains
+
+resource "cloudflare_record" "terraform_managed_resource_8ff6114729e0ccc038188c7349c44f93" {
+  name     = "mail"
+  priority = 10
+  proxied  = false
+  ttl      = 1
+  type     = "MX"
+  value    = "in1-smtp.messagingengine.com"
+  zone_id  = "994692a638e9a8c4ae552a9e6dacb609"
+}
+
+resource "cloudflare_record" "terraform_managed_resource_5d0d2e817cffc85eba40bec17a6fd6d9" {
+  name     = "mail"
+  priority = 20
+  proxied  = false
+  ttl      = 1
+  type     = "MX"
+  value    = "in2-smtp.messagingengine.com"
+  zone_id  = "994692a638e9a8c4ae552a9e6dacb609"
+}
+
+## SPF
+
+resource "cloudflare_record" "terraform_managed_resource_bf76208b81e67ff68b70a57bae94226f" {
+  name    = "hipparcos.net"
+  proxied = false
+  ttl     = 1
+  type    = "TXT"
+  value   = "v=spf1 include:spf.messagingengine.com ?all"
+  zone_id = "994692a638e9a8c4ae552a9e6dacb609"
+}
+
+## DKIM
+
 resource "cloudflare_record" "terraform_managed_resource_b861a72f5625b9e60f2610c85b08501b" {
   name    = "fm1._domainkey"
   proxied = false
@@ -33,77 +134,21 @@ resource "cloudflare_record" "terraform_managed_resource_be51b7f396ff3f68451c87e
   zone_id = "994692a638e9a8c4ae552a9e6dacb609"
 }
 
-resource "cloudflare_record" "terraform_managed_resource_29190c165c26da1d92c7858b74dab4d8" {
-  name    = "mail"
-  proxied = true
+## DMARC
+
+resource "cloudflare_record" "terraform_managed_resource_6364516621a4bfade37617e417b0c213" {
+  name    = "_dmarc"
+  proxied = false
   ttl     = 1
-  type    = "CNAME"
-  value   = "mail.fastmail.com"
+  type    = "TXT"
+  value   = "\"v=DMARC1;  p=none; rua=mailto:95506e4bcd13411f9c0dadfe1c2e7f85@dmarc-reports.cloudflare.net\""
   zone_id = "994692a638e9a8c4ae552a9e6dacb609"
 }
 
-resource "cloudflare_record" "terraform_managed_resource_8ce67ae8f8520e22b15daf5af245c5a3" {
-  name     = "*"
-  priority = 20
-  proxied  = false
-  ttl      = 1
-  type     = "MX"
-  value    = "in2-smtp.messagingengine.com"
-  zone_id  = "994692a638e9a8c4ae552a9e6dacb609"
-}
+## Client email auto-discovery
 
-resource "cloudflare_record" "terraform_managed_resource_6bc996fbe9aa45078bf34305ad1b6f50" {
-  name     = "*"
-  priority = 10
-  proxied  = false
-  ttl      = 1
-  type     = "MX"
-  value    = "in1-smtp.messagingengine.com"
-  zone_id  = "994692a638e9a8c4ae552a9e6dacb609"
-}
-
-resource "cloudflare_record" "terraform_managed_resource_80a3b9351b70b6c09085329d03c355ab" {
-  name     = "hipparcos.net"
-  priority = 20
-  proxied  = false
-  ttl      = 1
-  type     = "MX"
-  value    = "in2-smtp.messagingengine.com"
-  zone_id  = "994692a638e9a8c4ae552a9e6dacb609"
-}
-
-resource "cloudflare_record" "terraform_managed_resource_9a01f57d2026a498ba69c6e55b25e001" {
-  name     = "hipparcos.net"
-  priority = 10
-  proxied  = false
-  ttl      = 1
-  type     = "MX"
-  value    = "in1-smtp.messagingengine.com"
-  zone_id  = "994692a638e9a8c4ae552a9e6dacb609"
-}
-
-resource "cloudflare_record" "terraform_managed_resource_5d0d2e817cffc85eba40bec17a6fd6d9" {
-  name     = "mail"
-  priority = 20
-  proxied  = false
-  ttl      = 1
-  type     = "MX"
-  value    = "in2-smtp.messagingengine.com"
-  zone_id  = "994692a638e9a8c4ae552a9e6dacb609"
-}
-
-resource "cloudflare_record" "terraform_managed_resource_8ff6114729e0ccc038188c7349c44f93" {
-  name     = "mail"
-  priority = 10
-  proxied  = false
-  ttl      = 1
-  type     = "MX"
-  value    = "in1-smtp.messagingengine.com"
-  zone_id  = "994692a638e9a8c4ae552a9e6dacb609"
-}
-
-resource "cloudflare_record" "terraform_managed_resource_e7733a293362560c1279c946fa4a1da2" {
-  name     = "_caldavs._tcp"
+resource "cloudflare_record" "terraform_managed_resource_4c64611f8756ae7715ece35782342f0b" {
+  name     = "_submission._tcp"
   priority = 0
   proxied  = false
   ttl      = 1
@@ -111,17 +156,17 @@ resource "cloudflare_record" "terraform_managed_resource_e7733a293362560c1279c94
   zone_id  = "994692a638e9a8c4ae552a9e6dacb609"
   data {
     name     = "hipparcos.net"
-    port     = 443
+    port     = 587
     priority = 0
     proto    = "_tcp"
-    service  = "_caldavs"
-    target   = "caldav.fastmail.com"
+    service  = "_submission"
+    target   = "smtp.fastmail.com"
     weight   = 1
   }
 }
 
-resource "cloudflare_record" "terraform_managed_resource_3237bb1db82965b3796074caea68e40e" {
-  name     = "_caldav._tcp"
+resource "cloudflare_record" "terraform_managed_resource_88cc2e5ec9a93a44c7be32e9bb0a1323" {
+  name     = "_imap._tcp"
   priority = 0
   proxied  = false
   ttl      = 1
@@ -132,43 +177,7 @@ resource "cloudflare_record" "terraform_managed_resource_3237bb1db82965b3796074c
     port     = 0
     priority = 0
     proto    = "_tcp"
-    service  = "_caldav"
-    target   = "."
-    weight   = 0
-  }
-}
-
-resource "cloudflare_record" "terraform_managed_resource_6dbef040592eb401babb8ab4ebca1415" {
-  name     = "_carddavs._tcp"
-  priority = 0
-  proxied  = false
-  ttl      = 1
-  type     = "SRV"
-  zone_id  = "994692a638e9a8c4ae552a9e6dacb609"
-  data {
-    name     = "hipparcos.net"
-    port     = 443
-    priority = 0
-    proto    = "_tcp"
-    service  = "_carddavs"
-    target   = "carddav.fastmail.com"
-    weight   = 1
-  }
-}
-
-resource "cloudflare_record" "terraform_managed_resource_7482e7a3b939e94a17168e781bbc401a" {
-  name     = "_carddav._tcp"
-  priority = 0
-  proxied  = false
-  ttl      = 1
-  type     = "SRV"
-  zone_id  = "994692a638e9a8c4ae552a9e6dacb609"
-  data {
-    name     = "hipparcos.net"
-    port     = 0
-    priority = 0
-    proto    = "_tcp"
-    service  = "_carddav"
+    service  = "_imap"
     target   = "."
     weight   = 0
   }
@@ -192,8 +201,9 @@ resource "cloudflare_record" "terraform_managed_resource_daeb805f3d837ea3f13aedf
   }
 }
 
-resource "cloudflare_record" "terraform_managed_resource_88cc2e5ec9a93a44c7be32e9bb0a1323" {
-  name     = "_imap._tcp"
+
+resource "cloudflare_record" "terraform_managed_resource_4332f8e9dccbfaf72fcccb92a1bdff5b" {
+  name     = "_pop3._tcp"
   priority = 0
   proxied  = false
   ttl      = 1
@@ -204,27 +214,9 @@ resource "cloudflare_record" "terraform_managed_resource_88cc2e5ec9a93a44c7be32e
     port     = 0
     priority = 0
     proto    = "_tcp"
-    service  = "_imap"
+    service  = "_pop3"
     target   = "."
     weight   = 0
-  }
-}
-
-resource "cloudflare_record" "terraform_managed_resource_363ab94d5d812b41b1d99238b8c55496" {
-  name     = "_jmap._tcp"
-  priority = 0
-  proxied  = false
-  ttl      = 1
-  type     = "SRV"
-  zone_id  = "994692a638e9a8c4ae552a9e6dacb609"
-  data {
-    name     = "hipparcos.net"
-    port     = 443
-    priority = 0
-    proto    = "_tcp"
-    service  = "_jmap"
-    target   = "api.fastmail.com"
-    weight   = 1
   }
 }
 
@@ -246,8 +238,28 @@ resource "cloudflare_record" "terraform_managed_resource_0eeba5fa76750609ba994cf
   }
 }
 
-resource "cloudflare_record" "terraform_managed_resource_4332f8e9dccbfaf72fcccb92a1bdff5b" {
-  name     = "_pop3._tcp"
+resource "cloudflare_record" "terraform_managed_resource_363ab94d5d812b41b1d99238b8c55496" {
+  name     = "_jmap._tcp"
+  priority = 0
+  proxied  = false
+  ttl      = 1
+  type     = "SRV"
+  zone_id  = "994692a638e9a8c4ae552a9e6dacb609"
+  data {
+    name     = "hipparcos.net"
+    port     = 443
+    priority = 0
+    proto    = "_tcp"
+    service  = "_jmap"
+    target   = "api.fastmail.com"
+    weight   = 1
+  }
+}
+
+## Client CardDAV auto-discovery
+
+resource "cloudflare_record" "terraform_managed_resource_7482e7a3b939e94a17168e781bbc401a" {
+  name     = "_carddav._tcp"
   priority = 0
   proxied  = false
   ttl      = 1
@@ -258,14 +270,14 @@ resource "cloudflare_record" "terraform_managed_resource_4332f8e9dccbfaf72fcccb9
     port     = 0
     priority = 0
     proto    = "_tcp"
-    service  = "_pop3"
+    service  = "_carddav"
     target   = "."
     weight   = 0
   }
 }
 
-resource "cloudflare_record" "terraform_managed_resource_4c64611f8756ae7715ece35782342f0b" {
-  name     = "_submission._tcp"
+resource "cloudflare_record" "terraform_managed_resource_6dbef040592eb401babb8ab4ebca1415" {
+  name     = "_carddavs._tcp"
   priority = 0
   proxied  = false
   ttl      = 1
@@ -273,38 +285,49 @@ resource "cloudflare_record" "terraform_managed_resource_4c64611f8756ae7715ece35
   zone_id  = "994692a638e9a8c4ae552a9e6dacb609"
   data {
     name     = "hipparcos.net"
-    port     = 587
+    port     = 443
     priority = 0
     proto    = "_tcp"
-    service  = "_submission"
-    target   = "smtp.fastmail.com"
+    service  = "_carddavs"
+    target   = "carddav.fastmail.com"
     weight   = 1
   }
 }
 
-resource "cloudflare_record" "terraform_managed_resource_6364516621a4bfade37617e417b0c213" {
-  name    = "_dmarc"
-  proxied = false
-  ttl     = 1
-  type    = "TXT"
-  value   = "\"v=DMARC1;  p=none; rua=mailto:95506e4bcd13411f9c0dadfe1c2e7f85@dmarc-reports.cloudflare.net\""
-  zone_id = "994692a638e9a8c4ae552a9e6dacb609"
+## Client CalDAV auto-discovery
+
+resource "cloudflare_record" "terraform_managed_resource_3237bb1db82965b3796074caea68e40e" {
+  name     = "_caldav._tcp"
+  priority = 0
+  proxied  = false
+  ttl      = 1
+  type     = "SRV"
+  zone_id  = "994692a638e9a8c4ae552a9e6dacb609"
+  data {
+    name     = "hipparcos.net"
+    port     = 0
+    priority = 0
+    proto    = "_tcp"
+    service  = "_caldav"
+    target   = "."
+    weight   = 0
+  }
 }
 
-resource "cloudflare_record" "terraform_managed_resource_bf76208b81e67ff68b70a57bae94226f" {
-  name    = "hipparcos.net"
-  proxied = false
-  ttl     = 1
-  type    = "TXT"
-  value   = "v=spf1 include:spf.messagingengine.com ?all"
-  zone_id = "994692a638e9a8c4ae552a9e6dacb609"
-}
-
-resource "cloudflare_record" "terraform_managed_resource_f7e7d606ea20e6a29c7e268c7715b74e" {
-  name    = "hipparcos.net"
-  proxied = true
-  ttl     = 1
-  type    = "AAAA"
-  value   = "100::"
-  zone_id = "994692a638e9a8c4ae552a9e6dacb609"
+resource "cloudflare_record" "terraform_managed_resource_e7733a293362560c1279c946fa4a1da2" {
+  name     = "_caldavs._tcp"
+  priority = 0
+  proxied  = false
+  ttl      = 1
+  type     = "SRV"
+  zone_id  = "994692a638e9a8c4ae552a9e6dacb609"
+  data {
+    name     = "hipparcos.net"
+    port     = 443
+    priority = 0
+    proto    = "_tcp"
+    service  = "_caldavs"
+    target   = "caldav.fastmail.com"
+    weight   = 1
+  }
 }
--- tf/net_starjunk.tf
+++ tf/net_starjunk.tf
@@ -1,6 +1,6 @@
 moved {
   from = cloudflare_zone.terraform_managed_resource_acff21a4b43636283cd28bd5c5bd44a5
-  to = cloudflare_zone.starjunk_net
+  to   = cloudflare_zone.starjunk_net
 }
 
 resource "cloudflare_zone" "starjunk_net" {
@@ -58,7 +58,7 @@ resource "cloudflare_record" "terraform_managed_resource_8c0fbc31ad098d40f9eccb8
 
 moved {
   from = cloudflare_record.terraform_managed_resource_96ab3ddbd1d6bda8e41ad733053b18aa
-  to = cloudflare_record.aaaa
+  to   = cloudflare_record.aaaa
 }
 
 resource "cloudflare_record" "aaaa" {
@@ -72,7 +72,7 @@ resource "cloudflare_record" "aaaa" {
 
 moved {
   from = cloudflare_record.terraform_managed_resource_1d02e7ffa92156a8528ad4263f5462c0
-  to = cloudflare_record.www_aaaa
+  to   = cloudflare_record.www_aaaa
 }
 
 resource "cloudflare_record" "www_aaaa" {
@@ -81,62 +81,6 @@ resource "cloudflare_record" "www_aaaa" {
   ttl     = 1
   type    = "AAAA"
   value   = "100::"
-  zone_id = "acff21a4b43636283cd28bd5c5bd44a5"
-}
-
-resource "cloudflare_record" "terraform_managed_resource_96494382737d342503bf7f2f6aa83332" {
-  name    = "fm1._domainkey"
-  proxied = false
-  ttl     = 1
-  type    = "CNAME"
-  value   = "fm1.starjunk.net.dkim.fmhosted.com"
-  zone_id = "acff21a4b43636283cd28bd5c5bd44a5"
-}
-
-resource "cloudflare_record" "terraform_managed_resource_1f45cbb0881abba9d2dff8072d550aae" {
-  name    = "fm2._domainkey"
-  proxied = false
-  ttl     = 1
-  type    = "CNAME"
-  value   = "fm2.starjunk.net.dkim.fmhosted.com"
-  zone_id = "acff21a4b43636283cd28bd5c5bd44a5"
-}
-
-resource "cloudflare_record" "terraform_managed_resource_9714dc1aa362bbdf836abd7354b82252" {
-  name    = "fm3._domainkey"
-  proxied = false
-  ttl     = 1
-  type    = "CNAME"
-  value   = "fm3.starjunk.net.dkim.fmhosted.com"
-  zone_id = "acff21a4b43636283cd28bd5c5bd44a5"
-}
-
-resource "cloudflare_record" "terraform_managed_resource_cfdd0862380adbfdc5a7442b0c243536" {
-  name     = "starjunk.net"
-  priority = 20
-  proxied  = false
-  ttl      = 1
-  type     = "MX"
-  value    = "in2-smtp.messagingengine.com"
-  zone_id  = "acff21a4b43636283cd28bd5c5bd44a5"
-}
-
-resource "cloudflare_record" "terraform_managed_resource_08f5abc0c8ab5c7f626df016d605c6ac" {
-  name     = "starjunk.net"
-  priority = 10
-  proxied  = false
-  ttl      = 1
-  type     = "MX"
-  value    = "in1-smtp.messagingengine.com"
-  zone_id  = "acff21a4b43636283cd28bd5c5bd44a5"
-}
-
-resource "cloudflare_record" "terraform_managed_resource_b80a3533e30542aa54abcac7747f8e41" {
-  name    = "_dmarc"
-  proxied = false
-  ttl     = 1
-  type    = "TXT"
-  value   = "v=DMARC1;  p=none; rua=mailto:11e643eadb6648a8b81fc11bcfe022f9@dmarc-reports.cloudflare.net"
   zone_id = "acff21a4b43636283cd28bd5c5bd44a5"
 }
 
@@ -149,11 +93,354 @@ resource "cloudflare_record" "terraform_managed_resource_37e047cce80565276348d26
   zone_id = "acff21a4b43636283cd28bd5c5bd44a5"
 }
 
-resource "cloudflare_record" "terraform_managed_resource_73ef1766328832460724a6e3e780a257" {
+# Fastmail
+#
+# https://www.fastmail.help/hc/en-us/articles/360060591153-Manual-DNS-configuration
+
+moved {
+  from = cloudflare_record.terraform_managed_resource_08f5abc0c8ab5c7f626df016d605c6ac
+  to   = cloudflare_record.net_starjunk_fastmail_mx_1
+}
+
+resource "cloudflare_record" "net_starjunk_fastmail_mx_1" {
+  name     = "starjunk.net"
+  priority = 10
+  proxied  = false
+  ttl      = 1
+  type     = "MX"
+  value    = "in1-smtp.messagingengine.com"
+  zone_id  = "acff21a4b43636283cd28bd5c5bd44a5"
+}
+
+moved {
+  from = cloudflare_record.terraform_managed_resource_cfdd0862380adbfdc5a7442b0c243536
+  to   = cloudflare_record.net_starjunk_fastmail_mx_2
+}
+
+resource "cloudflare_record" "net_starjunk_fastmail_mx_2" {
+  name     = "starjunk.net"
+  priority = 20
+  proxied  = false
+  ttl      = 1
+  type     = "MX"
+  value    = "in2-smtp.messagingengine.com"
+  zone_id  = "acff21a4b43636283cd28bd5c5bd44a5"
+}
+
+## Subdomain Mail
+
+resource "cloudflare_record" "net_starjunk_fastmail_mail_mx_1" {
+  name     = "*"
+  priority = 10
+  proxied  = false
+  ttl      = 1
+  type     = "MX"
+  value    = "in1-smtp.messagingengine.com"
+  zone_id  = "acff21a4b43636283cd28bd5c5bd44a5"
+}
+
+resource "cloudflare_record" "net_starjunk_fastmail_mail_mx_2" {
+  name     = "*"
+  priority = 20
+  proxied  = false
+  ttl      = 1
+  type     = "MX"
+  value    = "in2-smtp.messagingengine.com"
+  zone_id  = "acff21a4b43636283cd28bd5c5bd44a5"
+}
+
+## Webmail Login Portal
+
+resource "cloudflare_record" "net_starjunk_fastmail_webmail_login_portal" {
+  name    = "mail"
+  proxied = true
+  ttl     = 1
+  type    = "CNAME"
+  value   = "mail.fastmail.com"
+  zone_id = "acff21a4b43636283cd28bd5c5bd44a5"
+}
+
+### Allow mail at subdomains
+
+resource "cloudflare_record" "net_starjunk_fastmail_subdomains_mx_1" {
+  name     = "mail"
+  priority = 10
+  proxied  = false
+  ttl      = 1
+  type     = "MX"
+  value    = "in1-smtp.messagingengine.com"
+  zone_id  = "acff21a4b43636283cd28bd5c5bd44a5"
+}
+
+resource "cloudflare_record" "net_starjunk_fastmail_subdomains_mx_2" {
+  name     = "mail"
+  priority = 20
+  proxied  = false
+  ttl      = 1
+  type     = "MX"
+  value    = "in2-smtp.messagingengine.com"
+  zone_id  = "acff21a4b43636283cd28bd5c5bd44a5"
+}
+
+## SPF
+
+moved {
+  from = cloudflare_record.terraform_managed_resource_73ef1766328832460724a6e3e780a257
+  to   = cloudflare_record.net_starjunk_spf
+}
+
+resource "cloudflare_record" "net_starjunk_spf" {
   name    = "starjunk.net"
   proxied = false
   ttl     = 1
   type    = "TXT"
   value   = "v=spf1 include:spf.messagingengine.com ?all"
   zone_id = "acff21a4b43636283cd28bd5c5bd44a5"
+}
+
+## DKIM
+
+moved {
+  from = cloudflare_record.terraform_managed_resource_96494382737d342503bf7f2f6aa83332
+  to   = cloudflare_record.net_starjunk_fastmail_dkim_1
+}
+
+resource "cloudflare_record" "net_starjunk_fastmail_dkim_1" {
+  name    = "fm1._domainkey"
+  proxied = false
+  ttl     = 1
+  type    = "CNAME"
+  value   = "fm1.starjunk.net.dkim.fmhosted.com"
+  zone_id = "acff21a4b43636283cd28bd5c5bd44a5"
+}
+
+moved {
+  from = cloudflare_record.terraform_managed_resource_1f45cbb0881abba9d2dff8072d550aae
+  to   = cloudflare_record.net_starjunk_fastmail_dkim_2
+}
+
+resource "cloudflare_record" "net_starjunk_fastmail_dkim_2" {
+  name    = "fm2._domainkey"
+  proxied = false
+  ttl     = 1
+  type    = "CNAME"
+  value   = "fm2.starjunk.net.dkim.fmhosted.com"
+  zone_id = "acff21a4b43636283cd28bd5c5bd44a5"
+}
+
+moved {
+  from = cloudflare_record.terraform_managed_resource_9714dc1aa362bbdf836abd7354b82252
+  to   = cloudflare_record.net_starjunk_fastmail_dkim_3
+}
+
+resource "cloudflare_record" "net_starjunk_fastmail_dkim_3" {
+  name    = "fm3._domainkey"
+  proxied = false
+  ttl     = 1
+  type    = "CNAME"
+  value   = "fm3.starjunk.net.dkim.fmhosted.com"
+  zone_id = "acff21a4b43636283cd28bd5c5bd44a5"
+}
+
+## DMARC
+
+moved {
+  from = cloudflare_record.terraform_managed_resource_b80a3533e30542aa54abcac7747f8e41
+  to   = cloudflare_record.net_starjunk_dmarc
+}
+
+resource "cloudflare_record" "net_starjunk_dmarc" {
+  name    = "_dmarc"
+  proxied = false
+  ttl     = 1
+  type    = "TXT"
+  value   = "v=DMARC1; p=none; rua=mailto:11e643eadb6648a8b81fc11bcfe022f9@dmarc-reports.cloudflare.net"
+  zone_id = "acff21a4b43636283cd28bd5c5bd44a5"
+}
+
+## Client email auto-discovery
+
+resource "cloudflare_record" "net_starjunk_fastmail_smtp" {
+  name     = "_submission._tcp"
+  priority = 0
+  proxied  = false
+  ttl      = 1
+  type     = "SRV"
+  zone_id  = "acff21a4b43636283cd28bd5c5bd44a5"
+  data {
+    name     = "starjunk.net"
+    port     = 587
+    priority = 0
+    proto    = "_tcp"
+    service  = "_submission"
+    target   = "smtp.fastmail.com"
+    weight   = 1
+  }
+}
+
+resource "cloudflare_record" "net_starjunk_fastmail_imap" {
+  name     = "_imap._tcp"
+  priority = 0
+  proxied  = false
+  ttl      = 1
+  type     = "SRV"
+  zone_id  = "acff21a4b43636283cd28bd5c5bd44a5"
+  data {
+    name     = "starjunk.net"
+    port     = 0
+    priority = 0
+    proto    = "_tcp"
+    service  = "_imap"
+    target   = "."
+    weight   = 0
+  }
+}
+
+resource "cloudflare_record" "net_starjunk_fastmail_imaps" {
+  name     = "_imaps._tcp"
+  priority = 0
+  proxied  = false
+  ttl      = 1
+  type     = "SRV"
+  zone_id  = "acff21a4b43636283cd28bd5c5bd44a5"
+  data {
+    name     = "starjunk.net"
+    port     = 993
+    priority = 0
+    proto    = "_tcp"
+    service  = "_imaps"
+    target   = "imap.fastmail.com"
+    weight   = 1
+  }
+}
+
+
+resource "cloudflare_record" "net_starjunk_fastmail_pop3" {
+  name     = "_pop3._tcp"
+  priority = 0
+  proxied  = false
+  ttl      = 1
+  type     = "SRV"
+  zone_id  = "acff21a4b43636283cd28bd5c5bd44a5"
+  data {
+    name     = "starjunk.net"
+    port     = 0
+    priority = 0
+    proto    = "_tcp"
+    service  = "_pop3"
+    target   = "."
+    weight   = 0
+  }
+}
+
+resource "cloudflare_record" "net_starjunk_fastmail_pop3s" {
+  name     = "_pop3s._tcp"
+  priority = 10
+  proxied  = false
+  ttl      = 1
+  type     = "SRV"
+  zone_id  = "acff21a4b43636283cd28bd5c5bd44a5"
+  data {
+    name     = "starjunk.net"
+    port     = 995
+    priority = 10
+    proto    = "_tcp"
+    service  = "_pop3s"
+    target   = "pop.fastmail.com"
+    weight   = 1
+  }
+}
+
+resource "cloudflare_record" "net_starjunk_fastmail_jmap" {
+  name     = "_jmap._tcp"
+  priority = 0
+  proxied  = false
+  ttl      = 1
+  type     = "SRV"
+  zone_id  = "acff21a4b43636283cd28bd5c5bd44a5"
+  data {
+    name     = "starjunk.net"
+    port     = 443
+    priority = 0
+    proto    = "_tcp"
+    service  = "_jmap"
+    target   = "api.fastmail.com"
+    weight   = 1
+  }
+}
+
+## Client CardDAV auto-discovery
+
+resource "cloudflare_record" "net_starjunk_fastmail_carddav" {
+  name     = "_carddav._tcp"
+  priority = 0
+  proxied  = false
+  ttl      = 1
+  type     = "SRV"
+  zone_id  = "acff21a4b43636283cd28bd5c5bd44a5"
+  data {
+    name     = "starjunk.net"
+    port     = 0
+    priority = 0
+    proto    = "_tcp"
+    service  = "_carddav"
+    target   = "."
+    weight   = 0
+  }
+}
+
+resource "cloudflare_record" "net_starjunk_fastmail_carddavs" {
+  name     = "_carddavs._tcp"
+  priority = 0
+  proxied  = false
+  ttl      = 1
+  type     = "SRV"
+  zone_id  = "acff21a4b43636283cd28bd5c5bd44a5"
+  data {
+    name     = "starjunk.net"
+    port     = 443
+    priority = 0
+    proto    = "_tcp"
+    service  = "_carddavs"
+    target   = "carddav.fastmail.com"
+    weight   = 1
+  }
+}
+
+## Client CalDAV auto-discovery
+
+resource "cloudflare_record" "net_starjunk_fastmail_caldav" {
+  name     = "_caldav._tcp"
+  priority = 0
+  proxied  = false
+  ttl      = 1
+  type     = "SRV"
+  zone_id  = "acff21a4b43636283cd28bd5c5bd44a5"
+  data {
+    name     = "starjunk.net"
+    port     = 0
+    priority = 0
+    proto    = "_tcp"
+    service  = "_caldav"
+    target   = "."
+    weight   = 0
+  }
+}
+
+resource "cloudflare_record" "net_starjunk_fastmail_caldavs" {
+  name     = "_caldavs._tcp"
+  priority = 0
+  proxied  = false
+  ttl      = 1
+  type     = "SRV"
+  zone_id  = "acff21a4b43636283cd28bd5c5bd44a5"
+  data {
+    name     = "starjunk.net"
+    port     = 443
+    priority = 0
+    proto    = "_tcp"
+    service  = "_caldavs"
+    target   = "caldav.fastmail.com"
+    weight   = 1
+  }
 }
